3ffa225214537c6cef079e3571fab616d0aa6b49
Borkhalenko Oleg <borkhalenko@gmail.com>
2021-01-17 22:53:49 +0200

    created project structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from "react-router-dom/modules/Link";
+
+function Navbar() {
+    return(
+        <div className="navbar">
+            <Link to="#" className="menu-bars">Link1</Link>
+        </div>
+    )
+}
+
+export default Navbar
